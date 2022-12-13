7d6e979a11fb58cddbe5579f0da7bbc0ad939fbf
Sarthak Sharma <sarthaks@tepper.cmu.edu>
2022-12-12 21:05:43 -0800

    update
    update

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/sarthaksharma/sarthaksharma.github.io.git";
+const repoURL = "https://github.com/sarthaksharma/home.git";
 
 ghpages.publish(
   pathname,
